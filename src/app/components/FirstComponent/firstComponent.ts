e13b30802170c1b81eea8e256cca6e1ec1170443
Dmytro Klivenko <dklivenko@epam.com>
2021-09-14 04:05:59 -0700

    Task1_4

--- src/app/components/FirstComponent/firstComponent.ts
+++ src/app/components/FirstComponent/firstComponent.ts
@@ -7,8 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FirstComponent implements OnInit {
   name!: string;
+  description!: string;
+  price!: number;
+  category!: Array<CategoriesEnum>;
+  isAvailable!: boolean;
 
   ngOnInit(): void {
     this.name = 'Geek Dreams';
+    this.description = 'Shop with sweets';
+    this.price = 100;
+    this.category = [CategoriesEnum.CHOCOLATES, CategoriesEnum.WAFFLES];
+    this.isAvailable = true;
   }
+}
+
+enum CategoriesEnum {
+  CHOCOLATES = "Chocolates",
+  CANDIES = "Candies",
+  MARSHMALLOW = "Marshmallow",
+  WAFFLES = "Waffles"
 }
