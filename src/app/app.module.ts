861b44dcd837816407913117e79ace936cc81864
Dmytro Klivenko <dklivenko@epam.com>
2021-09-14 04:14:25 -0700

    Task1_4 renamed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import {FirstModule} from "./components/FirstComponent/firstComponent.module";
+import {FirstModule} from "./components/firstComponent/firstComponent.module";
 
 @NgModule({
   declarations: [
